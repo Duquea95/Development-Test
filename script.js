103749099d529cd77a6eabe5c6d70496fcb80ad3
Anthony Duque <duquea95@gmail.com>
2018-06-06 02:40:30 -0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(function(){
     var i = 0;
+    var a = 0;
     var subMenu = $(".dropdown-toggle");
+    var itemName = $(".item-name");
     var obj = {
         collection: {
             "title1": "St. Louis Classics",
@@ -40,22 +42,70 @@ $(function(){
             "title35": "Toulouse",
             "title36": "Louisa",
             "title37": "Gift Card"
+        },
+        names:{
+            "title1": "Bengal Polarized 24k",
+            "title2": "Matte Oyster 24k",
+            "title3": "Matte Tokyo Tortoise 24k",
+            "title4": "Oyster To Black Polarized",
+            "title5": "Tobacco To Champagne Polarized 24k",
+            "title6": "Butterscotch to Black 24k",
+            "title7": "Ivy 24k",
+            "title8": "Matte AU Lait To Clear 24k",
+            "title9": "Matte Blonde",
+            "title10": "Blonde Tortoise To Champagne 24k",
+            "title11": "Matte Sunday Tortoise 24k",
+            "title12": "Matte Black 24k",
+            "title13": "Neptune 24k",
+            "title14": "Matte Cosmo to Blush",
+            "title15": "Galactic To Blush Rose Gold",
+            "title16": "Seaglass To Marine Rose Gold",
+            "title17": "Crystal 24k",
+            "title18": "Bengal Blue 24k",
+            "title19": "Camellia 24k",
+            "title20": "Matte Audubon 24k",
+            "title21": "Audubon Rose",
+            "title22": "Stardust To Champagne 24k",
+            "title23": "Matte Ciel To Blue Crystal",
+            "title24": "Bone To Black",
+            "title25":"Matte Isle To Champagne 24k",
+            "title26":"Blue Steel To Burgundy",
+            "title27": "Matte Bengal",
+            "title28": "White Linen 24k",
+            "title29": "Interstellar",
+            "title30": "Matte Plume To Palm 24k",
+            "title31": "Stella to Magnolia",
+            "title32": "Crystal",
+            "title33": "Blue Steel To Crystal 24k",
+            "title34": "Confetti To Marine 24k",
+            "title35": "24k Titanium + Zulu",
+            "title36": "Matte Black To 24k Titanium Stella",
+            "title37": "Matte Black Titanium Rex",
+            "title38": "Titanium + White Linen"
+            // "title39": "Titanium + White Linen"
         }
-
     }
+
     // desktop menu
-    // subMenu.on("mouseenter", function(){
+    // $(".nav-list-level-1-item").on("mouseover", function(){
     //     console.log($(this).children(".dropdown-menu"));
     //     $(this).children(".dropdown-menu").css("visibility", 'visible');
     // })
     // mouseleave: function() {
-    //     $(".dropdown-menu").css("visibility", 'none');
     // }
+    $(".nav-list-level-1-item").on({
+        mouseenter: function() {
+            $(this).children(".dropdown-menu").css("visibility", 'visible');
+        },
+        mouseleave: function() {
+            $(this).children(".dropdown-menu").css("visibility", 'none');
+        }
+    });
 
 
 
     // Mobile menu
-    subMenu.on("click mouseenter", function(){
+    subMenu.on("click", function(){
             var sub = $(this).text();
             if($(this).next().hasClass('open-sub')){
                 $(".open-sub").removeClass("open-sub");
@@ -68,27 +118,38 @@ $(function(){
     })
 
     for (var set in obj) {
-        // console.log(set);
-        var sets = obj[set]
-        for (var title in sets){
-            createCollection(sets[title])
+        if(set == "collection"){
+            var sets = obj[set];
+            // console.log(sets);
+            for (var title in sets){
+                createCollection(sets[title]);
+            }
+            i = 0;
+        }
+        else if(set == "names") {
+            var styles = obj[set];
+            for (var name in styles){
+                createStyle(styles[name]);
+            }
         }
     }
 
-    $(".open-js").on("click",function(){
-        $(".navbar-nav").addClass("navbar-nav-open");
-        $(".hamburger-menu").css("display", "none");
-        $(".close-js").removeClass("display-none");
-        $("main").css({"opacty": "1", "display": "none"});
-    });
-
-    $(".close-js").on("click",function(){
-        $(".navbar-nav").removeClass("navbar-nav-open");
-        $(".hamburger-menu").css("display", "block");
-        $(".close-js").addClass("display-none");
-        $("main").css({"opacty": "0", "display": "block"});
-    });
-
+    function createStyle(name){
+        if(a == 0){
+            // console.log(name);
+            var itemName = $(".item-name")
+            console.log(itemName[a]);
+            $(itemName[a]).addClass("name"+a);
+            $(itemName[a]).text(name);
+        }
+        else{
+            var itemName = $(".item-name")
+            $(itemName[a]).addClass("name"+a);
+            $(itemName[a]).text(name);
+        }
+        a++;
+        // console.log(a);
+    }
 
     function createCollection(title){
         if(i == 0){
@@ -104,9 +165,20 @@ $(function(){
             productSet.appendTo('div.mobile-display');
         }
         i++;
-
-
-
+        // console.log(i);
     }
 
+    $(".open-js").on("click",function(){
+        $(".navbar-nav").addClass("navbar-nav-open");
+        $(".hamburger-menu").css("display", "none");
+        $(".close-js").removeClass("display-none");
+        $("main").css({"opacty": "1", "display": "none"});
+    });
+
+    $(".close-js").on("click",function(){
+        $(".navbar-nav").removeClass("navbar-nav-open");
+        $(".hamburger-menu").css("display", "block");
+        $(".close-js").addClass("display-none");
+        $("main").css({"opacty": "0", "display": "block"});
+    });
 });
